10f787518fc93c2420e7794090b850d5aa7931cf
Nicolas Küchler <nico.kuechler@gmail.com>
2015-10-19 17:57:10 +0200

    Added pseudocode comments in MapPresenterImpl or how to implement the methods

--- project/se7en/src/ch/uzh/se/se7en/client/mvp/presenters/impl/MapPresenterImpl.java
+++ project/se7en/src/ch/uzh/se/se7en/client/mvp/presenters/impl/MapPresenterImpl.java
@@ -1,5 +1,7 @@
 package ch.uzh.se.se7en.client.mvp.presenters.impl;
 
+import org.gwtbootstrap3.extras.slider.client.ui.Range;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.HasWidgets;
 
@@ -39,25 +41,34 @@ public class MapPresenterImpl implements MapPresenter {
 	@Override
 	public void onRangeSliderChanged() {
 		// TODO Auto-generated method stub
-		
+		//Call update GeoChart
 	}
 
 	@Override
 	public void onCountrySelected() {
 		// TODO Auto-generated method stub
-		
+		// 1. clear genreTable & genrePieChart (or from within filmDataModel)
+		// 2. get year range from view
+		// 3. get Information which Country was selected (getGeoChartSelection() tbd if row index or what)
+		// 4. searchGenre(range.getMinValue, range.getMaxValue, row) from FilmDataModel
 	}
 
 	@Override
 	public void updateGeoChart() {
 		// TODO Auto-generated method stub
-		
+		// 1. clear Old geoChart ?
+		// 2. get year range from view
+		// 3. getCountries() from FilmDataModel tbd where conversion with year range list-> datatable takes places
+		// 4. setGeoChart(dataTable) from view
 	}
 
 	@Override
 	public void updateGenre() {
 		// TODO Auto-generated method stub
-		
+		// ? clearOldGenreStuff
+		// 1. is called from filmDataModel when rpc call from searchGenre is back
+		// 2. setGenreTable(filmDataModel.getGenreList());
+		// 3. setGenrePieChart(filmDataModel.getGenreDataTable());
 	}
 
 	@Override
