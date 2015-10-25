d4162d09872a758a7b84186b09338df812a36e58
Roland Schläfli <rolandschlaefli@gmail.com>
2015-10-25 16:20:36 +0100

    Annotate class, reformat

--- project/se7en/src/ch/uzh/se/se7en/shared/model/Genre.java
+++ project/se7en/src/ch/uzh/se/se7en/shared/model/Genre.java
@@ -1,53 +1,91 @@
 package ch.uzh.se.se7en.shared.model;
 
 import java.io.Serializable;
-/**
- Container to hold information about a genre.
- Is used to transport the genreData between client and server.
-@author Nicolas Küchler
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
-*/
+/**
+ * Container to hold information about a genre. Is used to transport the
+ * genreData between client and server.
+ * 
+ * @author Nicolas Küchler, Roland Schläfli
+ * 
+ */
+@Entity
+@Table(name = "genres")
 public class Genre implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
 	private String name;
+
+	@Transient
 	private int numberOfFilms;
-	
+
 	/**
-	@pre name!= null
-	@post -
-	@return the name
+	 * @pre id!= null
+	 * @post -
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @pre -
+	 * @post id==id
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @pre name!= null
+	 * @post -
+	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
-	@pre -
-	@post name==name
-	@param name the name to set
-	*/
+	 * @pre -
+	 * @post name==name
+	 * @param name
+	 *            the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	/**
-	@pre numberOfFilms!= null
-	@post -
-	@return the numberOfFilms
+	 * @pre numberOfFilms!= null
+	 * @post -
+	 * @return the numberOfFilms
 	 */
 	public int getNumberOfFilms() {
 		return numberOfFilms;
 	}
-	
+
 	/**
-	@pre -
-	@post numberOfFilms==numberOfFilms
-	@param numberOfFilms the numberOfFilms to set
-	*/
+	 * @pre -
+	 * @post numberOfFilms==numberOfFilms
+	 * @param numberOfFilms
+	 *            the numberOfFilms to set
+	 */
 	public void setNumberOfFilms(int numberOfFilms) {
 		this.numberOfFilms = numberOfFilms;
-	}	
-
+	}
 }
