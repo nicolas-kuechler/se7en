3ac8cfb0f8da7a4196441c3d0ce5ba4b4cf050dc
cyrillhalter <cyrill.halter@me.com>
2015-10-26 17:12:18 +0100

    Comments added

--- project/se7en/src/ch/uzh/se/se7en/server/model/TriggerImportServiceImpl.java
+++ project/se7en/src/ch/uzh/se/se7en/server/model/TriggerImportServiceImpl.java
@@ -19,9 +19,21 @@ import com.googlecode.jcsv.reader.internal.CSVReaderBuilder;
 import ch.uzh.se.se7en.client.rpc.TriggerImportService;
 
 import ch.uzh.se.se7en.shared.model.Film;
-
+/**
+ * Used to import film data from csv files in the Google Cloud Storage file repository
+ * @author Cyrill Halter
+ *
+ */
 public class TriggerImportServiceImpl extends RemoteServiceServlet implements TriggerImportService {
-
+	
+	/**
+	 * This method is called to to import a file from the Google Cloud Storage file repository and
+	 * convert it to a list of film objects
+	 *@author Cyrill Halter
+	 *@pre	A correctly formatted csv file has been deposited in the Cloud Storage
+	 *@post -
+	 *@param String nameOfFile The name of the csv file that should be imported
+	 */
 	@Override
 	public boolean importFile(String nameOfFile) {
 		GcsService gcsService = GcsServiceFactory.createGcsService();
