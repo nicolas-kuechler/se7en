b286f9f069d39a24e5498a0210f4c798c3d52e5a
cyrillhalter <cyrill.halter@me.com>
2015-10-26 15:58:03 +0100

    Method for importing files via TriggerImportService

--- project/se7en/src/ch/uzh/se/se7en/server/model/TriggerImportServiceImpl.java
+++ project/se7en/src/ch/uzh/se/se7en/server/model/TriggerImportServiceImpl.java
@@ -1,14 +1,49 @@
 package ch.uzh.se.se7en.server.model;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.channels.Channels;
+import java.util.List;
+
+import com.google.appengine.tools.cloudstorage.GcsFilename;
+import com.google.appengine.tools.cloudstorage.GcsInputChannel;
+import com.google.appengine.tools.cloudstorage.GcsService;
+import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.googlecode.jcsv.annotations.internal.ValueProcessorProvider;
+import com.googlecode.jcsv.reader.CSVReader;
+import com.googlecode.jcsv.reader.internal.AnnotationEntryParser;
+import com.googlecode.jcsv.reader.internal.CSVReaderBuilder;
 
 import ch.uzh.se.se7en.client.rpc.TriggerImportService;
+
+import ch.uzh.se.se7en.shared.model.Film;
 
 public class TriggerImportServiceImpl extends RemoteServiceServlet implements TriggerImportService {
 
 	@Override
 	public boolean importFile(String nameOfFile) {
-		// TODO Auto-generated method stub
+		GcsService gcsService = GcsServiceFactory.createGcsService();
+		GcsFilename gcsFilename = new GcsFilename("se-team-se7en", nameOfFile);
+		List<Film> importedFilms = null;
+		
+		try{
+			
+			GcsInputChannel csvReadChannel = gcsService.openReadChannel(gcsFilename, 0);
+			Reader csvFileReader = new InputStreamReader(Channels.newInputStream(csvReadChannel));
+			
+			ValueProcessorProvider vpp = new ValueProcessorProvider();
+			CSVReader<Film> filmReader = new CSVReaderBuilder<Film>(csvFileReader).
+					entryParser(new AnnotationEntryParser<Film>(Film.class, vpp)).build();
+			importedFilms = filmReader.readAll();
+			
+		}catch (IOException e){
+			e.printStackTrace();
+			return false;
+		}
+		
+		//insert method for adding films to DB here
 		return true;
 	}
 
