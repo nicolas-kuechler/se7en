807846bd24fb915f67e416712dbb2ad095470337
Roland Schläfli <rolandschlaefli@gmail.com>
2015-10-31 15:42:27 +0100

    Move the Country database logic into a separate file, add toString()

---
+++ project/se7en/src/ch/uzh/se/se7en/server/model/CountryDB.java
@@ -0,0 +1,123 @@
+package ch.uzh.se.se7en.server.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import ch.uzh.se.se7en.server.model.FilmDB;
+import ch.uzh.se.se7en.shared.model.Country;
+import ch.uzh.se.se7en.shared.model.DTO;
+
+/**
+ * Container for fetching all Country related data from the DB via hibernate.
+ * 
+ * @author Roland Schläfli
+ */
+@Entity
+@Table(name = "countries")
+public class CountryDB implements DTO {
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "code")
+	private String code;
+	
+	@ManyToMany(targetEntity = FilmDB.class)
+	private Set<FilmDB> films;
+	
+	/**
+	 * Converts this entity to a data transfer object
+	 * 
+	 * @pre -
+	 * @post -
+	 * @return Country The CountryDB entity converted to a country data transfer object
+	 */
+	public Country toCountry() {
+		// TODO: fill with the real number of films
+		int [] numOfFilms = {0, 1, 2, 3};
+
+		return new Country(id, name, code, numOfFilms);
+	}
+
+	/**
+	@pre id!= null
+	@post -
+	@return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	@pre -
+	@post id==id
+	@param id the id to set
+	*/
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	@pre name!= null
+	@post -
+	@return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	@pre -
+	@post name==name
+	@param name the name to set
+	*/
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	@pre code!= null
+	@post -
+	@return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	@pre -
+	@post code==code
+	@param code the code to set
+	*/
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	@pre films!= null
+	@post -
+	@return the films
+	 */
+	public Set<FilmDB> getFilms() {
+		return films;
+	}
+
+	/**
+	@pre -
+	@post films==films
+	@param films the films to set
+	*/
+	public void setFilms(Set<FilmDB> films) {
+		this.films = films;
+	}
+}
