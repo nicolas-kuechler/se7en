b65d3b077381a4a79ffc93e5bc2f412c30cc268c
Roland Schläfli <rolandschlaefli@gmail.com>
2015-10-31 15:43:25 +0100

    Move the db logic into a separate file, add ManyToMany relationships, add toString and setToStringList

--- project/se7en/src/ch/uzh/se/se7en/shared/model/Film.java
+++ project/se7en/src/ch/uzh/se/se7en/shared/model/Film.java
@@ -1,202 +1,187 @@
 package ch.uzh.se.se7en.shared.model;
 
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-
-import com.googlecode.jcsv.annotations.MapToColumn;
+import java.util.List;
 
 /**
  * Container to hold the film data on client side and server side. Is used to
  * transport the movie data between client and server.
  * 
- * @author Nicolas Küchler, Roland Schläfli
+ * @author Nicolas Küchler
  */
-@Entity
-@Table(name = "movies")
-public class Film implements Serializable {
+public class Film implements Serializable, DTO {
 
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue
-	@Column(name = "id")
 	private int id;
-
-	@Column(name = "name")
-	@MapToColumn(column=0)
 	private String name;
-
-	@Column(name = "length", nullable=true)
-	@MapToColumn(column=1)
 	private Integer length;
-	
-	// TODO: will be in a different relation
-	@Column(name = "country", nullable=true)
-	@MapToColumn(column=2)
-	private String country;
-		
-	// TODO: will be in a different relation
-	@Column(name = "language", nullable=true)
-	@MapToColumn(column=3)
-	private String language;
-	
-	@Column(name = "year", nullable=true)
-	@MapToColumn(column=4)
 	private Integer year;
-	
-	// TODO: will be in a different relation
-	@Column(name = "genre", nullable=true)
-	@MapToColumn(column=5)
-	private String genre;
-	
-	public Film()
-	{
-		
+	private List<String> countries;
+	private List<String> languages;
+	private List<String> genres;
+
+	public Film() {
+
 	}
-	
-	public Film(String name)
-	{
-		setName(name);
+
+	public Film(String name) {
+		this.name = name;
 	}
-	
-	public Film(String name, int length, String country, String language, int year, String genre) 
-	{
+
+	public Film(int id, String name, int length, int year, List<String> countries, List<String> languages,
+			List<String> genres) {
+		this.id = id;
 		this.name = name;
 		this.length = length;
-		this.country = country;
-		this.language = language;
+		this.countries = countries;
+		this.languages = languages;
 		this.year = year;
-		this.genre = genre;
+		this.genres = genres;
 	}
 
 	/**
-	@pre id!= null
-	@post -
-	@return the id
+	 * Returns a string representation of this Film
+	 * 
+	 * @author Roland Schläfli
+	 * @pre -
+	 * @post -
+	 * @return String The string representation of this instance
+	 */
+	@Override
+	public String toString() {
+		return "Id: " + id + " - Name: " + name + " - Länge: " + length + " - Länder: " + countries + " - Sprachen: " + languages + " - Genres: " + genres;
+	}
+	
+	/**
+	 * @pre id!= null
+	 * @post -
+	 * @return the id
 	 */
 	public int getId() {
 		return id;
 	}
 
 	/**
-	@pre -
-	@post id==id
-	@param id the id to set
-	*/
+	 * @pre -
+	 * @post id==id
+	 * @param id
+	 *            the id to set
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
 	/**
-	@pre name!= null
-	@post -
-	@return the name
+	 * @pre name!= null
+	 * @post -
+	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
 
 	/**
-	@pre -
-	@post name==name
-	@param name the name to set
-	*/
+	 * @pre -
+	 * @post name==name
+	 * @param name
+	 *            the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
 	/**
-	@pre length!= null
-	@post -
-	@return the length
+	 * @pre length!= null
+	 * @post -
+	 * @return the length
 	 */
 	public Integer getLength() {
 		return length;
 	}
 
 	/**
-	@pre -
-	@post length==length
-	@param length the length to set
-	*/
+	 * @pre -
+	 * @post length==length
+	 * @param length
+	 *            the length to set
+	 */
 	public void setLength(Integer length) {
 		this.length = length;
 	}
 
 	/**
-	@pre year!= null
-	@post -
-	@return the year
+	 * @pre year!= null
+	 * @post -
+	 * @return the year
 	 */
 	public Integer getYear() {
 		return year;
 	}
 
 	/**
-	@pre -
-	@post year==year
-	@param year the year to set
-	*/
+	 * @pre -
+	 * @post year==year
+	 * @param year
+	 *            the year to set
+	 */
 	public void setYear(Integer year) {
 		this.year = year;
 	}
 
 	/**
-	@pre country!= null
-	@post -
-	@return the country
+	 * @pre countries!= null
+	 * @post -
+	 * @return the countries
 	 */
-	public String getCountry() {
-		return country;
+	public List<String> getCountries() {
+		return countries;
 	}
 
 	/**
-	@pre -
-	@post country==country
-	@param country the country to set
-	*/
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	/**
-	@pre language!= null
-	@post -
-	@return the language
+	 * @pre -
+	 * @post countries==countries
+	 * @param countries
+	 *            the countries to set
 	 */
-	public String getLanguage() {
-		return language;
+	public void setCountries(List<String> countries) {
+		this.countries = countries;
 	}
 
 	/**
-	@pre -
-	@post language==language
-	@param language the language to set
-	*/
-	public void setLanguage(String language) {
-		this.language = language;
-	}
-
-	/**
-	@pre genre!= null
-	@post -
-	@return the genre
+	 * @pre languages!= null
+	 * @post -
+	 * @return the languages
 	 */
-	public String getGenre() {
-		return genre;
+	public List<String> getLanguages() {
+		return languages;
 	}
 
 	/**
-	@pre -
-	@post genre==genre
-	@param genre the genre to set
-	*/
-	public void setGenre(String genre) {
-		this.genre = genre;
+	 * @pre -
+	 * @post languages==languages
+	 * @param languages
+	 *            the languages to set
+	 */
+	public void setLanguages(List<String> languages) {
+		this.languages = languages;
+	}
+
+	/**
+	 * @pre genres!= null
+	 * @post -
+	 * @return the genres
+	 */
+	public List<String> getGenres() {
+		return genres;
+	}
+
+	/**
+	 * @pre -
+	 * @post genres==genres
+	 * @param genres
+	 *            the genres to set
+	 */
+	public void setGenres(List<String> genres) {
+		this.genres = genres;
 	}
 }
