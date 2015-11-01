ead0926f74d70c59c696579db3ceea306ccb18d2
buenzlidominik <buenzlidominik@bluewin.ch>
2015-11-01 17:35:34 +0100

    Sorting & Design

--- project/se7en/src/ch/uzh/se/se7en/client/mvp/views/impl/TableViewImpl.java
+++ project/se7en/src/ch/uzh/se/se7en/client/mvp/views/impl/TableViewImpl.java
@@ -1,19 +1,25 @@
 package ch.uzh.se.se7en.client.mvp.views.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.ListGroup;
 import org.gwtbootstrap3.client.ui.ListGroupItem;
+import org.gwtbootstrap3.client.ui.gwt.ButtonCell;
 import org.gwtbootstrap3.client.ui.gwt.DataGrid;
 
+import com.google.appengine.labs.repackaged.com.google.common.collect.Lists;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.ColumnSortEvent;
 import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Window;
@@ -25,74 +31,91 @@ import ch.uzh.se.se7en.client.mvp.presenters.TablePresenter;
 import ch.uzh.se.se7en.client.mvp.views.TableView;
 import ch.uzh.se.se7en.shared.model.Film;
 
-public class TableViewImpl extends Composite implements TableView{
+public class TableViewImpl extends Composite implements TableView {
 
 	private static TableViewImplUiBinder uiBinder = GWT.create(TableViewImplUiBinder.class);
 
 	interface TableViewImplUiBinder extends UiBinder<Widget, TableViewImpl> {
 	}
-	
+
 	private TablePresenter tablePresenter;
-	
-	private TextColumn sortColumn;
-	String cwDataGridEmpty = "IS EMPTY";
+
 	private String concatString;
+	private List<Film> films;
+	private int sortDesc = -1;
 	/**
 	 * The main DataGrid.
 	 */
 	@UiField(provided = true)
 	DataGrid<Film> dataGrid;
 
-	@UiHandler("downloadBtn")
+	@UiHandler("downloadButton")
 	public void onDownloadBtnClicked(final ClickEvent event) {
 		tablePresenter.onDownloadStarted();
 	}
 
-	/**
-	 * The pager used to change the range of data.
-	 */
-	// @UiField(provided = true)
-	// SimplePager pager;
+	@UiHandler("sortButton")
+	public void onSortButtonClicked(final ClickEvent event) {
+		callSort();
+	}
 
-	/**
-	 * Initialize this example.
-	 */
+	public void callSort() {
+		if (sortDesc == -1 || sortDesc == 0) {
+			Collections.sort(films, new Comparator<Film>() {
+				public int compare(Film f1, Film f2) {
+					if (f1 == f2) {
+						return 0;
+					}
+
+					if (f1 != null) {
+						if (f2 != null) {
+							return f1.getName().compareTo(f2.getName());
+						}
+					}
+					return -1;
+				}
+			});
+			sortDesc = 1;
+
+		} else {
+			Collections.reverse(films);
+			sortDesc = 0;
+		}
+
+		dataGrid.setRowData(0, films);
+	}
 
 	public TableViewImpl() {
 
-		// DataGrid<Film> dataGrid = new DataGrid<Film>();
 		dataGrid = new DataGrid<Film>();
 		dataGrid.setWidth("100%");
-		dataGrid.setHeight("500px");
+		dataGrid.setHeight("400px");
 		dataGrid.setBordered(true);
 		dataGrid.setAutoHeaderRefreshDisabled(true);
-		
+
 		buildTable();
 
 		initWidget(uiBinder.createAndBindUi(this));
 
-		// TODO Setup Enter Pressed for start search
 	}
 
-	private String listToString(List<String> list){
-		
+	private String listToString(List<String> list) {
+
 		concatString = "";
-		int iterator=0;
-		for(String str: list){
-			if(iterator == 0){
+		int iterator = 0;
+		for (String str : list) {
+			if (iterator == 0) {
 				concatString = str;
 				iterator++;
-			}else{
-				concatString = concatString + " / " + str  ;
+			} else {
+				concatString = concatString + " / " + str;
 				iterator++;
 			}
-			
+
 		}
 		return concatString;
 	}
-	/**
-	 * Add the columns to the table.
-	 */
+
 	private void buildTable() {
 
 		TextColumn<Film> nameColumn = new TextColumn<Film>() {
@@ -101,11 +124,6 @@ public class TableViewImpl extends Composite implements TableView{
 				return filmObject.getName();
 			}
 		};
-		
-		sortColumn = nameColumn;
-		nameColumn.setSortable(true);
-		
-
 
 		TextColumn<Film> lengthColumn = new TextColumn<Film>() {
 			@Override
@@ -114,14 +132,12 @@ public class TableViewImpl extends Composite implements TableView{
 			}
 		};
 
-
 		TextColumn<Film> countryColumn = new TextColumn<Film>() {
 			@Override
 			public String getValue(Film filmObject) {
 				return listToString(filmObject.getCountries());
 			}
 		};
-
 
 		TextColumn<Film> languageColumn = new TextColumn<Film>() {
 			@Override
@@ -130,7 +146,6 @@ public class TableViewImpl extends Composite implements TableView{
 			}
 		};
 
-
 		TextColumn<Film> yearColumn = new TextColumn<Film>() {
 			@Override
 			public String getValue(Film filmObject) {
@@ -138,14 +153,13 @@ public class TableViewImpl extends Composite implements TableView{
 			}
 		};
 
-
 		TextColumn<Film> genreColumn = new TextColumn<Film>() {
 			@Override
 			public String getValue(Film filmObject) {
 				return listToString(filmObject.getGenres());
 			}
 		};
-		
+
 		dataGrid.setColumnWidth(nameColumn, 16.66, Unit.PCT);
 		dataGrid.addColumn(nameColumn, "Name");
 		dataGrid.setColumnWidth(lengthColumn, 16.66, Unit.PCT);
@@ -156,10 +170,9 @@ public class TableViewImpl extends Composite implements TableView{
 		dataGrid.addColumn(languageColumn, "Language");
 		dataGrid.setColumnWidth(yearColumn, 16.66, Unit.PCT);
 		dataGrid.addColumn(yearColumn, "Year");
-		dataGrid.setColumnWidth(genreColumn,16.66 , Unit.PCT);
+		dataGrid.setColumnWidth(genreColumn, 16.66, Unit.PCT);
 		dataGrid.addColumn(genreColumn, "Genre");
-		
-		
+
 	}
 
 	@Override
@@ -168,36 +181,12 @@ public class TableViewImpl extends Composite implements TableView{
 	}
 
 	@Override
-	public void setTable(List<Film> films) {
-		
-		ListHandler<Film> columnSortHandler = new ListHandler<Film>(films);
-	    columnSortHandler.setComparator(sortColumn,new Comparator<Film>() {
-	          public int compare(Film f1, Film f2) {
-	            if (f1 == f2) {
-	              return 0;
-	            }
-
-	            // Compare the name columns.
-	            if (f1 != null) {
-	            	if (f2!=null){
-	            		dataGrid.redraw(); 
-	            		return f1.getName().compareTo(f2.getName());
-	            	}
-	            }
-	            return -1;
-	          }
-	        });
-	    
-	    dataGrid.addColumnSortHandler(columnSortHandler);
-	    // We know that the data is sorted alphabetically by default.
-	    dataGrid.getColumnSortList().push(sortColumn);
-	    dataGrid.redraw(); 
+	public void setTable(List<Film> filme) {
+		films = filme;
 		dataGrid.setRowCount(films.size(), true);
 		dataGrid.setRowData(0, films);
-
+		callSort();
 	}
-	
-
 
 	@Override
 	public void startDownload(String downloadUrl) {
