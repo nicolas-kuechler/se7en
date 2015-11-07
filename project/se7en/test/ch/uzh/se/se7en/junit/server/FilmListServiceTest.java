c57715e574f852daf367baecc99efad886b08d54
Roland Schläfli <rolandschlaefli@gmail.com>
2015-11-07 22:25:05 +0100

    Implement tests for getCountryEntitiesList, getGenreSelectOption, getCountrySelectOption, getLanguageSelectOption

--- project/se7en/test/ch/uzh/se/se7en/junit/server/FilmListServiceTest.java
+++ project/se7en/test/ch/uzh/se/se7en/junit/server/FilmListServiceTest.java
@@ -1,12 +1,13 @@
 package ch.uzh.se.se7en.junit.server;
 
 import static ch.uzh.se.se7en.junit.server.TestUtil.mockQuery;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,6 +38,7 @@ import ch.uzh.se.se7en.server.model.LanguageDB;
 import ch.uzh.se.se7en.shared.model.Country;
 import ch.uzh.se.se7en.shared.model.Film;
 import ch.uzh.se.se7en.shared.model.FilmFilter;
+import ch.uzh.se.se7en.shared.model.SelectOption;
 
 /**
  * Test for the answering of rpc calls
@@ -62,11 +64,13 @@ public class FilmListServiceTest {
 	Set<FilmCountryDB> dbFilmCountries = new HashSet<FilmCountryDB>();
 	Set<FilmGenreDB> dbFilmGenres = new HashSet<FilmGenreDB>();
 	Set<FilmLanguageDB> dbFilmLanguages = new HashSet<FilmLanguageDB>();
-	
+
 	// create fake queries
 	TypedQuery<FilmDB> filmQuery = mockQuery(dbFilms);
 	TypedQuery<CountryDB> countryQuery = mockQuery(dbCountries);
-	
+	TypedQuery<GenreDB> genreQuery = mockQuery(dbGenres);
+	TypedQuery<LanguageDB> languageQuery = mockQuery(dbLanguages);
+
 	@Before
 	public void setup() {
 		// need to do this manually for mocking a generic
@@ -76,13 +80,13 @@ public class FilmListServiceTest {
 		dbCountries.add(new CountryDB("Switzerland"));
 		dbGenres.add(new GenreDB("Horror"));
 		dbLanguages.add(new LanguageDB("German"));
-		
+
 		FilmDB fakeFilm = new FilmDB("Film 1", 90, 2015);
-		
+
 		dbFilmCountries.add(new FilmCountryDB(fakeFilm, dbCountries.get(0)));
 		dbFilmGenres.add(new FilmGenreDB(fakeFilm, dbGenres.get(0)));
 		dbFilmLanguages.add(new FilmLanguageDB(fakeFilm, dbLanguages.get(0)));
-		
+
 		fakeFilm.setFilmCountryEntities(dbFilmCountries);
 		fakeFilm.setFilmGenreEntities(dbFilmGenres);
 		fakeFilm.setFilmLanguageEntities(dbFilmLanguages);
@@ -91,7 +95,9 @@ public class FilmListServiceTest {
 		// the mocked queries should return the fake results
 		doReturn(filmQuery).when(manager).createQuery(Matchers.anyString(), Matchers.same(FilmDB.class));
 		doReturn(countryQuery).when(manager).createQuery(Matchers.anyString(), Matchers.same(CountryDB.class));
-		
+		doReturn(genreQuery).when(manager).createQuery(Matchers.anyString(), Matchers.same(GenreDB.class));
+		doReturn(languageQuery).when(manager).createQuery(Matchers.anyString(), Matchers.same(LanguageDB.class));
+
 		// mock the call get() on the provider
 		doReturn(manager).when(em).get();
 
@@ -101,10 +107,10 @@ public class FilmListServiceTest {
 
 	@Test
 	public void testGetFilmList() {
-		/* INITIALIZATION BLOCK */	
+		/* INITIALIZATION BLOCK */
 		// filter with the default filters
 		FilmFilter filter = new FilmFilter();
-				
+
 		/* EXECUTION BLOCK */
 		List<Film> films = rpcService.getFilmList(filter);
 		Film film = films.get(0);
@@ -150,35 +156,6 @@ public class FilmListServiceTest {
 
 	@Test
 	public void testGetFilmEntitiesListFilterByName() {
-		/* INITIALIZATION BLOCK */
-		// filter only by name
-		FilmFilter filter = new FilmFilter("Hallo");
-
-		/* EXECUTION BLOCK */
-		List<FilmDB> films = rpcService.getFilmEntitiesList(filter);
-
-		/* VERIFICATION BLOCK */
-		// verify that the correct query string is generated
-		verify(manager, times(1)).createQuery(
-				"SELECT DISTINCT f FROM FilmDB f WHERE (f.length BETWEEN :minLength AND :maxLength) AND (f.year BETWEEN :minYear AND :maxYear) AND LOWER(f.name) LIKE :findName ORDER BY f.name",
-				FilmDB.class);
-
-		// verify that all the parameters are correctly set
-		verify(filmQuery, times(1)).setParameter("minLength", filter.getLengthStart());
-		verify(filmQuery, times(1)).setParameter("maxLength", filter.getLengthEnd());
-		verify(filmQuery, times(1)).setParameter("minYear", filter.getYearStart());
-		verify(filmQuery, times(1)).setParameter("maxYear", filter.getYearEnd());
-		verify(filmQuery, times(1)).setParameter("findName", "%hallo%");
-
-		// verify that the query is then executed
-		verify(filmQuery, times(1)).getResultList();
-
-		// verify that the query isn't touched any further
-		verifyNoMoreInteractions(filmQuery);
-	}
-
-	@Test
-	public void testGetFilmEntitiesListFilterByNameAndCountries() {
 		/* INITIALIZATION BLOCK */
 		// set the filters
 		FilmFilter filter = new FilmFilter("Hallo");
@@ -259,47 +236,189 @@ public class FilmListServiceTest {
 		/* INITIALIZATION BLOCK */
 		// filter with the default filters
 		FilmFilter filter = new FilmFilter();
-		int[] numOfFilms = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
-		
+		int[] numOfFilms = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
+
 		/* EXECUTION BLOCK */
 		List<Country> countries = rpcService.getCountryList(filter);
 		Country country = countries.get(0);
 
 		/* VERIFICATION BLOCK */
 		assertEquals(country.getName(), "Switzerland");
-		
+
 		// assert that the number of films array was correctly generated
 		assertTrue(Arrays.equals(country.getNumberOfFilms(), numOfFilms));
 	}
 
 	@Test
-	public void testGetCountryEntitiesList() {
+	public void testGetCountryEntitiesListFilteredDefault() {
 		/* INITIALIZATION BLOCK */
-		// return the country list on any call
-		doReturn(mockQuery(dbCountries)).when(manager).createQuery(Matchers.anyString(),
-				Matchers.same(CountryDB.class));
+		// filter with the default filters
+		FilmFilter filter = new FilmFilter();
+
+		/* EXECUTION BLOCK */
+		List<CountryDB> countries = rpcService.getCountryEntitiesList(filter);
 
 		/* VERIFICATION BLOCK */
-		// execute without any filters
-		rpcService.getCountryEntitiesList(new FilmFilter());
+		// verify that the correct query string is generated
+		verify(manager, times(1)).createQuery(
+				"SELECT DISTINCT c FROM CountryDB c JOIN c.filmCountryEntities fc JOIN fc.primaryKey.film fi WHERE (fi.length BETWEEN :minLength AND :maxLength) AND (fi.year BETWEEN :minYear AND :maxYear) ORDER BY c.name",
+				CountryDB.class);
+
+		// verify that all the parameters are correctly set
+		verify(countryQuery, times(1)).setParameter("minLength", filter.getLengthStart());
+		verify(countryQuery, times(1)).setParameter("maxLength", filter.getLengthEnd());
+		verify(countryQuery, times(1)).setParameter("minYear", filter.getYearStart());
+		verify(countryQuery, times(1)).setParameter("maxYear", filter.getYearEnd());
+
+		// verify that the query is then executed
+		verify(countryQuery, times(1)).getResultList();
+
+		// verify that the query isn't touched any further
+		verifyNoMoreInteractions(countryQuery);
+	}
+
+	@Test
+	public void testGetCountryEntitiesListFilteredByName() {
+		/* INITIALIZATION BLOCK */
+		// filter by name only
+		FilmFilter filter = new FilmFilter("Hallo");
+
+		/* EXECUTION BLOCK */
+		List<CountryDB> countries = rpcService.getCountryEntitiesList(filter);
+
+		/* VERIFICATION BLOCK */
+		// verify that the correct query string is generated
+		verify(manager, times(1)).createQuery(
+				"SELECT DISTINCT c FROM CountryDB c JOIN c.filmCountryEntities fc JOIN fc.primaryKey.film fi WHERE (fi.length BETWEEN :minLength AND :maxLength) AND (fi.year BETWEEN :minYear AND :maxYear) AND LOWER(fi.name) LIKE :findName ORDER BY c.name",
+				CountryDB.class);
+
+		// verify that all the parameters are correctly set
+		verify(countryQuery, times(1)).setParameter("minLength", filter.getLengthStart());
+		verify(countryQuery, times(1)).setParameter("maxLength", filter.getLengthEnd());
+		verify(countryQuery, times(1)).setParameter("minYear", filter.getYearStart());
+		verify(countryQuery, times(1)).setParameter("maxYear", filter.getYearEnd());
+		verify(countryQuery, times(1)).setParameter("findName", "%hallo%");
+
+		// verify that the query is then executed
+		verify(countryQuery, times(1)).getResultList();
+
+		// verify that the query isn't touched any further
+		verifyNoMoreInteractions(countryQuery);
+	}
+
+	@Test
+	public void testGetCountryEntitiesListFilteredByEverything() {
+		/* INITIALIZATION BLOCK */
+		// set the filters
+		FilmFilter filter = new FilmFilter();
+		Set<Integer> ids = new HashSet<Integer>();
+		ids.add(1);
+		ids.add(99);
+		filter.setName("Hallo");
+		filter.setLengthStart(11);
+		filter.setLengthEnd(22);
+		filter.setYearStart(2013);
+		filter.setYearEnd(2015);
+		filter.setCountryIds(ids); // should not have any effect
+		filter.setGenreIds(ids);
+		filter.setLanguageIds(ids);
+
+		/* EXECUTION BLOCK */
+		List<CountryDB> countries = rpcService.getCountryEntitiesList(filter);
+
+		/* VERIFICATION BLOCK */
+		// verify that the correct query string is generated
+		verify(manager, times(1)).createQuery(
+				"SELECT DISTINCT c FROM CountryDB c JOIN c.filmCountryEntities fc JOIN fc.primaryKey.film fi JOIN fi.filmGenreEntities fg JOIN fi.filmLanguageEntities fl WHERE (fi.length BETWEEN :minLength AND :maxLength) AND (fi.year BETWEEN :minYear AND :maxYear) AND LOWER(fi.name) LIKE :findName AND fg.genreId IN :genreIds AND fl.languageId IN :languageIds ORDER BY c.name",
+				CountryDB.class);
+
+		// verify that all the parameters are correctly set
+		verify(countryQuery, times(1)).setParameter("minLength", filter.getLengthStart());
+		verify(countryQuery, times(1)).setParameter("maxLength", filter.getLengthEnd());
+		verify(countryQuery, times(1)).setParameter("minYear", filter.getYearStart());
+		verify(countryQuery, times(1)).setParameter("maxYear", filter.getYearEnd());
+		verify(countryQuery, times(1)).setParameter("findName", "%hallo%");
+		verify(countryQuery, times(1)).setParameter("genreIds", ids);
+		verify(countryQuery, times(1)).setParameter("languageIds", ids);
+
+		// verify that the query is then executed
+		verify(countryQuery, times(1)).getResultList();
+
+		// verify that the query isn't touched any further
+		verifyNoMoreInteractions(countryQuery);
 	}
 
 	@Test
 	public void testGetGenreList() {
+		// TODO: Sprint 2
 	}
 
 	@Test
 	public void testGetGenreSelectOption() {
+		/* INITIALIZATION BLOCK */
 
+		/* EXECUTION BLOCK */
+		List<SelectOption> genreSelects = rpcService.getGenreSelectOption();
+
+		/* VERIFICATION BLOCK */
+		// verify that the correct query string is generated
+		verify(manager, times(1)).createQuery("FROM GenreDB ORDER BY name", GenreDB.class);
+
+		// verify that the query is then executed
+		verify(genreQuery, times(1)).getResultList();
+
+		// verify that the query isn't touched any further
+		verifyNoMoreInteractions(genreQuery);
+
+		// assert that the select options were properly created
+		assertEquals(genreSelects.get(0).getId(), new Integer(0));
+		assertEquals(genreSelects.get(0).getName(), "Horror");
 	}
 
 	@Test
 	public void testGetCountrySelectOption() {
+		/* INITIALIZATION BLOCK */
 
+		/* EXECUTION BLOCK */
+		List<SelectOption> countrySelects = rpcService.getCountrySelectOption();
+
+		/* VERIFICATION BLOCK */
+		// verify that the correct query string is generated
+		verify(manager, times(1)).createQuery("FROM CountryDB ORDER BY name", CountryDB.class);
+
+		// verify that the query is then executed
+		verify(countryQuery, times(1)).getResultList();
+
+		// verify that the query isn't touched any further
+		verifyNoMoreInteractions(countryQuery);
+
+		// assert that the select options were properly created
+		assertEquals(countrySelects.get(0).getId(), new Integer(0));
+		assertEquals(countrySelects.get(0).getName(), "Switzerland");
 	}
 
 	@Test
 	public void testGetLanguageSelectOption() {
+		/* INITIALIZATION BLOCK */
 
+		/* EXECUTION BLOCK */
+		List<SelectOption> languageSelects = rpcService.getLanguageSelectOption();
+
+		/* VERIFICATION BLOCK */
+		// verify that the correct query string is generated
+		verify(manager, times(1)).createQuery("FROM LanguageDB ORDER BY name", LanguageDB.class);
+
+		// verify that the query is then executed
+		verify(languageQuery, times(1)).getResultList();
+
+		// verify that the query isn't touched any further
+		verifyNoMoreInteractions(languageQuery);
+
+		// assert that the select options were properly created
+		assertEquals(languageSelects.get(0).getId(), new Integer(0));
+		assertEquals(languageSelects.get(0).getName(), "German");
 	}
 }
