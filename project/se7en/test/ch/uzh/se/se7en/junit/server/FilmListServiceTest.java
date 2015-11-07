4e0261bc46e09a8d6bc57277cd85cb272f3c08ee
Roland Schläfli <rolandschlaefli@gmail.com>
2015-11-07 21:42:37 +0100

    Implement tests for getFilmList and getFilmListEntities with different filters

--- project/se7en/test/ch/uzh/se/se7en/junit/server/FilmListServiceTest.java
+++ project/se7en/test/ch/uzh/se/se7en/junit/server/FilmListServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -26,9 +27,13 @@ import com.google.inject.Provider;
 
 import ch.uzh.se.se7en.server.FilmListServiceImpl;
 import ch.uzh.se.se7en.server.model.CountryDB;
+import ch.uzh.se.se7en.server.model.FilmCountryDB;
 import ch.uzh.se.se7en.server.model.FilmDB;
+import ch.uzh.se.se7en.server.model.FilmGenreDB;
+import ch.uzh.se.se7en.server.model.FilmLanguageDB;
 import ch.uzh.se.se7en.server.model.GenreDB;
 import ch.uzh.se.se7en.server.model.LanguageDB;
+import ch.uzh.se.se7en.shared.model.Film;
 import ch.uzh.se.se7en.shared.model.FilmFilter;
 
 /**
@@ -47,23 +52,44 @@ public class FilmListServiceTest {
 
 	Provider<EntityManager> em;
 
-	// create fake lists of all the entities in the database
+	// create fake lists/sets of all the entities in the database
 	List<FilmDB> dbFilms = new ArrayList<FilmDB>();
 	List<CountryDB> dbCountries = new ArrayList<CountryDB>();
 	List<GenreDB> dbGenres = new ArrayList<GenreDB>();
 	List<LanguageDB> dbLanguages = new ArrayList<LanguageDB>();
-
+	Set<FilmCountryDB> dbFilmCountries = new HashSet<FilmCountryDB>();
+	Set<FilmGenreDB> dbFilmGenres = new HashSet<FilmGenreDB>();
+	Set<FilmLanguageDB> dbFilmLanguages = new HashSet<FilmLanguageDB>();
+	
+	// create fake queries
+	TypedQuery<FilmDB> filmQuery = mockQuery(dbFilms);
+	TypedQuery<CountryDB> countryQuery = mockQuery(dbCountries);
+	
 	@Before
 	public void setup() {
 		// need to do this manually for mocking a generic
 		em = (Provider<EntityManager>) mock(Provider.class);
 
-		// add fake content to the entity lists
-		dbFilms.add(new FilmDB("Film 1", 90, 2015));
+		// add one fake film to the mocked database
 		dbCountries.add(new CountryDB("Switzerland"));
 		dbGenres.add(new GenreDB("Horror"));
 		dbLanguages.add(new LanguageDB("German"));
+		
+		FilmDB fakeFilm = new FilmDB("Film 1", 90, 2015);
+		
+		dbFilmCountries.add(new FilmCountryDB(fakeFilm, dbCountries.get(0)));
+		dbFilmGenres.add(new FilmGenreDB(fakeFilm, dbGenres.get(0)));
+		dbFilmLanguages.add(new FilmLanguageDB(fakeFilm, dbLanguages.get(0)));
+		
+		fakeFilm.setFilmCountryEntities(dbFilmCountries);
+		fakeFilm.setFilmGenreEntities(dbFilmGenres);
+		fakeFilm.setFilmLanguageEntities(dbFilmLanguages);
+		dbFilms.add(fakeFilm);
 
+		// the mocked queries should return the fake results
+		doReturn(filmQuery).when(manager).createQuery(Matchers.anyString(), Matchers.same(FilmDB.class));
+		doReturn(countryQuery).when(manager).createQuery(Matchers.anyString(), Matchers.same(CountryDB.class));
+		
 		// mock the call get() on the provider
 		doReturn(manager).when(em).get();
 
@@ -73,27 +99,33 @@ public class FilmListServiceTest {
 
 	@Test
 	public void testGetFilmList() {
-		/* INITIALIZATION BLOCK */
-
+		/* INITIALIZATION BLOCK */	
+		// filter with the default filters
+		FilmFilter filter = new FilmFilter();
+				
 		/* EXECUTION BLOCK */
-		// rpcService.getFilmList(new FilmFilter());
+		List<Film> films = rpcService.getFilmList(filter);
+		Film film = films.get(0);
 
 		/* VERIFICATION BLOCK */
-
+		// assert that all the information in the film is correctly set
+		assertEquals(films.size(), 1);
+		assertEquals(film.getName(), "Film 1");
+		assertEquals(film.getLength(), new Integer(90));
+		assertEquals(film.getYear(), new Integer(2015));
+		assertEquals(film.getCountries().get(0), "Switzerland");
+		assertEquals(film.getGenres().get(0), "Horror");
+		assertEquals(film.getLanguages().get(0), "German");
 	}
 
 	@Test
 	public void testGetFilmEntitiesListFilterDefault() {
 		/* INITIALIZATION BLOCK */
-		// return the fake film list on any call
-		TypedQuery<FilmDB> query = mockQuery(dbFilms);
-		doReturn(query).when(manager).createQuery(Matchers.anyString(), Matchers.same(FilmDB.class));
-
 		// filter with the default filters
 		FilmFilter filter = new FilmFilter();
 
 		/* EXECUTION BLOCK */
-		rpcService.getFilmEntitiesList(filter);
+		List<FilmDB> films = rpcService.getFilmEntitiesList(filter);
 
 		/* VERIFICATION BLOCK */
 		// verify that the correct query string is generated
@@ -102,30 +134,26 @@ public class FilmListServiceTest {
 				FilmDB.class);
 
 		// verify that all the parameters are correctly set
-		verify(query, times(1)).setParameter("minLength", filter.getLengthStart());
-		verify(query, times(1)).setParameter("maxLength", filter.getLengthEnd());
-		verify(query, times(1)).setParameter("minYear", filter.getYearStart());
-		verify(query, times(1)).setParameter("maxYear", filter.getYearEnd());
+		verify(filmQuery, times(1)).setParameter("minLength", filter.getLengthStart());
+		verify(filmQuery, times(1)).setParameter("maxLength", filter.getLengthEnd());
+		verify(filmQuery, times(1)).setParameter("minYear", filter.getYearStart());
+		verify(filmQuery, times(1)).setParameter("maxYear", filter.getYearEnd());
 
 		// verify that the query is then executed
-		verify(query, times(1)).getResultList();
+		verify(filmQuery, times(1)).getResultList();
 
 		// verify that the query isn't touched any further
-		verifyNoMoreInteractions(query);
+		verifyNoMoreInteractions(filmQuery);
 	}
 
 	@Test
 	public void testGetFilmEntitiesListFilterByName() {
 		/* INITIALIZATION BLOCK */
-		// return the fake film list on any call
-		TypedQuery<FilmDB> query = mockQuery(dbFilms);
-		doReturn(query).when(manager).createQuery(Matchers.anyString(), Matchers.same(FilmDB.class));
-
 		// filter only by name
 		FilmFilter filter = new FilmFilter("Hallo");
 
 		/* EXECUTION BLOCK */
-		rpcService.getFilmEntitiesList(filter);
+		List<FilmDB> films = rpcService.getFilmEntitiesList(filter);
 
 		/* VERIFICATION BLOCK */
 		// verify that the correct query string is generated
@@ -134,26 +162,22 @@ public class FilmListServiceTest {
 				FilmDB.class);
 
 		// verify that all the parameters are correctly set
-		verify(query, times(1)).setParameter("minLength", filter.getLengthStart());
-		verify(query, times(1)).setParameter("maxLength", filter.getLengthEnd());
-		verify(query, times(1)).setParameter("minYear", filter.getYearStart());
-		verify(query, times(1)).setParameter("maxYear", filter.getYearEnd());
-		verify(query, times(1)).setParameter("findName", "%hallo%");
+		verify(filmQuery, times(1)).setParameter("minLength", filter.getLengthStart());
+		verify(filmQuery, times(1)).setParameter("maxLength", filter.getLengthEnd());
+		verify(filmQuery, times(1)).setParameter("minYear", filter.getYearStart());
+		verify(filmQuery, times(1)).setParameter("maxYear", filter.getYearEnd());
+		verify(filmQuery, times(1)).setParameter("findName", "%hallo%");
 
 		// verify that the query is then executed
-		verify(query, times(1)).getResultList();
+		verify(filmQuery, times(1)).getResultList();
 
 		// verify that the query isn't touched any further
-		verifyNoMoreInteractions(query);
+		verifyNoMoreInteractions(filmQuery);
 	}
 
 	@Test
 	public void testGetFilmEntitiesListFilterByNameAndCountries() {
 		/* INITIALIZATION BLOCK */
-		// return the fake film list on any call
-		TypedQuery<FilmDB> query = mockQuery(dbFilms);
-		doReturn(query).when(manager).createQuery(Matchers.anyString(), Matchers.same(FilmDB.class));
-
 		// set the filters
 		FilmFilter filter = new FilmFilter("Hallo");
 		Set<Integer> countryIds = new HashSet<Integer>();
@@ -162,7 +186,7 @@ public class FilmListServiceTest {
 		filter.setCountryIds(countryIds);
 
 		/* EXECUTION BLOCK */
-		rpcService.getFilmEntitiesList(filter);
+		List<FilmDB> films = rpcService.getFilmEntitiesList(filter);
 
 		/* VERIFICATION BLOCK */
 		// verify that the correct query string is generated
@@ -171,27 +195,23 @@ public class FilmListServiceTest {
 				FilmDB.class);
 
 		// verify that all the parameters are correctly set
-		verify(query, times(1)).setParameter("minLength", filter.getLengthStart());
-		verify(query, times(1)).setParameter("maxLength", filter.getLengthEnd());
-		verify(query, times(1)).setParameter("minYear", filter.getYearStart());
-		verify(query, times(1)).setParameter("maxYear", filter.getYearEnd());
-		verify(query, times(1)).setParameter("findName", "%hallo%");
-		verify(query, times(1)).setParameter("countryIds", filter.getCountryIds());
+		verify(filmQuery, times(1)).setParameter("minLength", filter.getLengthStart());
+		verify(filmQuery, times(1)).setParameter("maxLength", filter.getLengthEnd());
+		verify(filmQuery, times(1)).setParameter("minYear", filter.getYearStart());
+		verify(filmQuery, times(1)).setParameter("maxYear", filter.getYearEnd());
+		verify(filmQuery, times(1)).setParameter("findName", "%hallo%");
+		verify(filmQuery, times(1)).setParameter("countryIds", filter.getCountryIds());
 
 		// verify that the query is then executed
-		verify(query, times(1)).getResultList();
+		verify(filmQuery, times(1)).getResultList();
 
 		// verify that the query isn't touched any further
-		verifyNoMoreInteractions(query);
+		verifyNoMoreInteractions(filmQuery);
 	}
 
 	@Test
 	public void testGetFilmEntitiesListFilterByEverything() {
 		/* INITIALIZATION BLOCK */
-		// return the fake film list on any call
-		TypedQuery<FilmDB> query = mockQuery(dbFilms);
-		doReturn(query).when(manager).createQuery(Matchers.anyString(), Matchers.same(FilmDB.class));
-
 		// set the filters
 		FilmFilter filter = new FilmFilter();
 		Set<Integer> ids = new HashSet<Integer>();
@@ -207,7 +227,7 @@ public class FilmListServiceTest {
 		filter.setLanguageIds(ids);
 
 		/* EXECUTION BLOCK */
-		rpcService.getFilmEntitiesList(filter);
+		List<FilmDB> films = rpcService.getFilmEntitiesList(filter);
 
 		/* VERIFICATION BLOCK */
 		// verify that the correct query string is generated
@@ -216,26 +236,28 @@ public class FilmListServiceTest {
 				FilmDB.class);
 
 		// verify that all the parameters are correctly set
-		verify(query, times(1)).setParameter("minLength", filter.getLengthStart());
-		verify(query, times(1)).setParameter("maxLength", filter.getLengthEnd());
-		verify(query, times(1)).setParameter("minYear", filter.getYearStart());
-		verify(query, times(1)).setParameter("maxYear", filter.getYearEnd());
-		verify(query, times(1)).setParameter("findName", "%hallo%");
-		verify(query, times(1)).setParameter("countryIds", filter.getCountryIds());
-		verify(query, times(1)).setParameter("genreIds", filter.getGenreIds());
-		verify(query, times(1)).setParameter("languageIds", filter.getLanguageIds());
+		verify(filmQuery, times(1)).setParameter("minLength", filter.getLengthStart());
+		verify(filmQuery, times(1)).setParameter("maxLength", filter.getLengthEnd());
+		verify(filmQuery, times(1)).setParameter("minYear", filter.getYearStart());
+		verify(filmQuery, times(1)).setParameter("maxYear", filter.getYearEnd());
+		verify(filmQuery, times(1)).setParameter("findName", "%hallo%");
+		verify(filmQuery, times(1)).setParameter("countryIds", filter.getCountryIds());
+		verify(filmQuery, times(1)).setParameter("genreIds", filter.getGenreIds());
+		verify(filmQuery, times(1)).setParameter("languageIds", filter.getLanguageIds());
 
 		// verify that the query is then executed
-		verify(query, times(1)).getResultList();
+		verify(filmQuery, times(1)).getResultList();
 
 		// verify that the query isn't touched any further
-		verifyNoMoreInteractions(query);
+		verifyNoMoreInteractions(filmQuery);
 	}
 
 	@Test
-	public void testGetCountryList() {
+	public void testGetCountryListFilterDefault() {
 		/* INITIALIZATION BLOCK */
-
+		// filter with the default filters
+		FilmFilter filter = new FilmFilter();
+		
 		/* EXECUTION BLOCK */
 
 		/* VERIFICATION BLOCK */
