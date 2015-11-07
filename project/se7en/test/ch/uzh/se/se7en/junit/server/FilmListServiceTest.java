6969cd1752e6923492703a55eaeafa6d89b6151e
Roland Schläfli <rolandschlaefli@gmail.com>
2015-11-07 18:33:13 +0100

    Stubs for FuilmListService and FilmListExportService tests

---
+++ project/se7en/test/ch/uzh/se/se7en/junit/server/FilmListServiceTest.java
@@ -0,0 +1,14 @@
+package ch.uzh.se.se7en.junit.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FilmListServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
