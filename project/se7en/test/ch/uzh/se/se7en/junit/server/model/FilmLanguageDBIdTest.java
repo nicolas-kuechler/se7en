eae23227553c3edcf177890aff6e9bebe435d7e7
Roland Schläfli <rolandschlaefli@gmail.com>
2015-11-08 10:48:22 +0100

    Stubs

---
+++ project/se7en/test/ch/uzh/se/se7en/junit/server/model/FilmLanguageDBIdTest.java
@@ -0,0 +1,14 @@
+package ch.uzh.se.se7en.junit.server.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FilmLanguageDBIdTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
