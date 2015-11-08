9e3704a1946920537b945d3feaf7f16d0d6f78a1
buenzlidominik <buenzlidominik@bluewin.ch>
2015-11-08 13:23:22 +0100

    DBID Test

--- project/se7en/test/ch/uzh/se/se7en/junit/server/model/FilmCountryDBIdTest.java
+++ project/se7en/test/ch/uzh/se/se7en/junit/server/model/FilmCountryDBIdTest.java
@@ -2,13 +2,53 @@ package ch.uzh.se.se7en.junit.server.model;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import ch.uzh.se.se7en.server.model.CountryDB;
+import ch.uzh.se.se7en.server.model.FilmCountryDB;
+import ch.uzh.se.se7en.server.model.FilmCountryDBId;
+import ch.uzh.se.se7en.server.model.FilmDB;
+
 public class FilmCountryDBIdTest {
+	
+	CountryDB country = new CountryDB("Absurdistan");
+	FilmDB film = new FilmDB("Hallo Welt", 10, 1993);
+	
+	@Test
+	public void testFilmCountryDBId() {
+		FilmCountryDBId countryDBId = new FilmCountryDBId();
+		assertEquals(countryDBId.getFilm(),null);
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetFilm() {
+		FilmCountryDBId countryDBId = new FilmCountryDBId();
+		countryDBId.setFilm(film);
+		assertEquals(countryDBId.getFilm(),film);
+	}
+
+	@Test
+	public void testSetFilm() {
+		FilmCountryDBId countryDBId = new FilmCountryDBId();
+		countryDBId.setFilm(film);
+		assertEquals(countryDBId.getFilm(),film);
+	}
+
+	@Test
+	public void testGetCountry() {
+		FilmCountryDBId countryDBId = new FilmCountryDBId();
+		countryDBId.setCountry(country);
+		assertEquals(countryDBId.getCountry(),country);
+	}
+
+	@Test
+	public void testSetCountry() {
+		FilmCountryDBId countryDBId = new FilmCountryDBId();
+		countryDBId.setCountry(country);
+		assertEquals(countryDBId.getCountry(),country);
 	}
 
 }
