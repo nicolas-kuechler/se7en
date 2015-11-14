f4a463b7023ea0688dcfac53d9623b952ee3150d
cyrillhalter <cyrill.halter@me.com>
2015-11-14 16:56:26 +0100

    Added FilmListExportService functionality; created FilmEntryConverter for jcsv library to convert Film objects to String arrays that can be written to the csv file

---
+++ project/se7en/src/ch/uzh/se/se7en/server/FilmEntryConverter.java
@@ -0,0 +1,84 @@
+package ch.uzh.se.se7en.server;
+
+import com.googlecode.jcsv.writer.CSVEntryConverter;
+
+import ch.uzh.se.se7en.shared.model.Film;
+
+public class FilmEntryConverter implements CSVEntryConverter<Film>{
+
+	@Override
+	public String[] convertEntry(Film film) {
+		String[] columns = new String[6];
+
+		//write name
+		if (film.getName() == null){
+			columns[0] = "";
+		}else{
+			columns[0] = film.getName();
+		}
+
+		//write length
+		if (film.getLength() == null){
+			columns[1] = "";
+		}else{
+			columns[1] = film.getLength().toString();
+		}
+
+		//write countries
+		if(film.getCountries() == null){
+			columns[2] = "";
+		}else{
+			String countries = "";
+			for (String country : film.getCountries()){
+				if(countries.equals("")){
+					countries = country;
+				}else{
+					countries = countries + "--" + country;
+				}
+			}
+			columns[2] = countries;
+		}
+
+		//write languages
+		if(film.getLanguages() == null ){
+			columns[3] = "";
+		}else{
+			String languages = "";
+			for (String language : film.getLanguages()){
+				if(languages.equals("")){
+					languages = language;
+				}else{
+					languages = languages + "--" + language;
+				}
+			}
+			columns[3] = languages;
+		}
+
+		//write year
+		if (film.getYear() == null){
+			columns[4] = "";
+		}else{
+			columns[4] = film.getYear().toString();
+		}
+
+		//write genres
+		if(film.getLanguages() == null ){
+			columns[3] = "";
+		}else{
+			String genres = "";
+			for (String genre : film.getGenres()){
+				if(genres.equals("")){
+					genres = genre;
+				}else{
+					genres = genres + "--" + genre;
+				}
+			}
+			columns[5] = genres;
+
+			
+		}
+		
+		return columns;
+	}
+
+}
--- project/se7en/src/ch/uzh/se/se7en/server/FilmListExportServiceImpl.java
+++ project/se7en/src/ch/uzh/se/se7en/server/FilmListExportServiceImpl.java
@@ -1,18 +1,72 @@
 package ch.uzh.se.se7en.server;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.util.UUID;
+
+import com.google.appengine.tools.cloudstorage.GcsFileOptions;
+import com.google.appengine.tools.cloudstorage.GcsFilename;
+import com.google.appengine.tools.cloudstorage.GcsOutputChannel;
+import com.google.appengine.tools.cloudstorage.GcsService;
+import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.google.inject.Singleton;
+import com.googlecode.jcsv.writer.CSVWriter;
+import com.googlecode.jcsv.writer.internal.CSVWriterBuilder;
 
 import ch.uzh.se.se7en.client.rpc.FilmListExportService;
+import ch.uzh.se.se7en.shared.model.Film;
 import ch.uzh.se.se7en.shared.model.FilmFilter;
 
 @Singleton
 public class FilmListExportServiceImpl extends RemoteServiceServlet implements FilmListExportService {
 
+	private final String BUCKET_NAME = "se-team-se7en";
 	@Override
 	public String getFilmListDownloadUrl(FilmFilter filter) {
-		// TODO Auto-generated method stub
-		return "Demo Download Url";
+		//create FilmListService
+		FilmListServiceImpl filmListService = new FilmListServiceImpl();
+		
+		//create GCS service
+		GcsService gcsService = GcsServiceFactory.createGcsService();
+		
+		//create unique file name
+		String uniqueFilename = "filtered_table_" + UUID.randomUUID().toString() + ".csv";
+		
+		//create GCS file name
+		GcsFilename filename = new GcsFilename(BUCKET_NAME, uniqueFilename);
+		
+		//create options for CSV file to be exported
+		GcsFileOptions.Builder builder = new GcsFileOptions.Builder();
+		
+		GcsFileOptions options = builder
+				.mimeType("text/csv")
+				.acl("public-read")
+				.build();
+		
+		//open channel for writing to file
+		GcsOutputChannel writeChannel = null;
+		try {
+			writeChannel = gcsService.createOrReplace(filename, options);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		BufferedWriter writer = new BufferedWriter(Channels.newWriter(writeChannel, "UTF-8"));
+		
+		//write films to CSV
+		CSVWriter<Film> csvWriter = new CSVWriterBuilder<Film>(writer).entryConverter(new FilmEntryConverter()).build();
+		try {
+			csvWriter.writeAll(filmListService.getFilmList(filter));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		String downloadURL = "https://storage.googleapis.com/" + BUCKET_NAME + uniqueFilename;
+		
+		return downloadURL;
 	}
 
 }
