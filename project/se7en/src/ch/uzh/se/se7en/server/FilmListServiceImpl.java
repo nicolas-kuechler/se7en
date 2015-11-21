80c596cb9113fc2ecee515e36608f222f0b16051
Roland Schläfli <rolandschlaefli@gmail.com>
2015-11-22 00:19:38 +0100

    Add optimized getCountryList, remove getCountryEntitiesList

--- project/se7en/src/ch/uzh/se/se7en/server/FilmListServiceImpl.java
+++ project/se7en/src/ch/uzh/se/se7en/server/FilmListServiceImpl.java
@@ -1,20 +1,14 @@
 package ch.uzh.se.se7en.server;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-
-import org.hibernate.annotations.QueryHints;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.google.inject.Inject;
@@ -24,9 +18,6 @@ import com.google.inject.persist.Transactional;
 
 import ch.uzh.se.se7en.client.rpc.FilmListService;
 import ch.uzh.se.se7en.server.model.CountryDB;
-import ch.uzh.se.se7en.server.model.CountryYearCountDB;
-import ch.uzh.se.se7en.server.model.CountryYearCountDBId;
-import ch.uzh.se.se7en.server.model.FilmCountryDB;
 import ch.uzh.se.se7en.server.model.FilmDB;
 import ch.uzh.se.se7en.server.model.GenreDB;
 import ch.uzh.se.se7en.server.model.LanguageDB;
@@ -45,7 +36,7 @@ public class FilmListServiceImpl extends RemoteServiceServlet implements FilmLis
 	// guice injection of an entity manager
 	@Inject
 	Provider<EntityManager> em;
-	
+
 	// TODO: find other way of caching stuff
 	FilmFilter cachedFilter;
 	List<FilmDB> cachedFilms;
@@ -72,6 +63,7 @@ public class FilmListServiceImpl extends RemoteServiceServlet implements FilmLis
 		List<Film> films = new ArrayList<Film>();
 
 		// convert each FilmDB instance to a Film DataTransferObject
+
 		for (FilmDB film : dbFilms) {
 			Film f = film.toFilm();
 			films.add(f);
@@ -92,11 +84,11 @@ public class FilmListServiceImpl extends RemoteServiceServlet implements FilmLis
 	 * @return List<FilmDB> dbFilms A filtered list of film entities
 	 */
 	@Transactional
-	public List<FilmDB> getFilmEntitiesList(FilmFilter filter) {	
-		if(cachedFilter != null && cachedFilms != null && filter.equals(cachedFilter)) {
+	public List<FilmDB> getFilmEntitiesList(FilmFilter filter) {
+		if (cachedFilter != null && cachedFilms != null && filter.equals(cachedFilter)) {
 			return cachedFilms;
 		}
-		
+
 		// the starting position of the query
 		// TODO: replace by filter information?
 		int startPosition = 0;
@@ -178,10 +170,11 @@ public class FilmListServiceImpl extends RemoteServiceServlet implements FilmLis
 
 		// make the query cacheable if it is select *
 		// TODO: query cache
-		/* if(wheres == "WHERE 1=1") {
-			query.setHint(QueryHints.CACHEABLE, true);
-		} */
-		
+		/*
+		 * if(wheres == "WHERE 1=1") { query.setHint(QueryHints.CACHEABLE,
+		 * true); }
+		 */
+
 		// set offset and limit
 		query.setFirstResult(startPosition);
 		query.setMaxResults(maxResults);
@@ -223,12 +216,12 @@ public class FilmListServiceImpl extends RemoteServiceServlet implements FilmLis
 
 		// execute the query
 		dbFilms = query.getResultList();
-		
+
 		// fill the local "cache"
 		// TODO: use another way of caching
 		cachedFilter = filter;
 		cachedFilms = dbFilms;
-		
+
 		// return the list of film entities
 		return dbFilms;
 	}
@@ -244,189 +237,112 @@ public class FilmListServiceImpl extends RemoteServiceServlet implements FilmLis
 	 * @return List<Country> countries The filtered list of country data
 	 *         transfer objects
 	 */
+	@Transactional
 	@Override
 	public List<Country> getCountryList(FilmFilter filter) {
 		// create an empty list of countries
-		List<CountryDB> dbCountries = new ArrayList<CountryDB>();
+		// List<CountryDB> dbCountries = new ArrayList<CountryDB>();
 		List<Country> countries = new ArrayList<Country>();
 
-		List<FilmDB> dbFilms = new ArrayList<FilmDB>();
-		dbFilms = getFilmEntitiesList(filter);
-		
-		Map<String, int[]> filmsPerYear = new HashMap<String, int[]>(250);
-		
-		// get the current year
-		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-				
-		// iterate over each film
-		for(FilmDB film : dbFilms) {
-			
-			// check if we have the necessary data to include the film in the calculation
-			// TODO: allow 2016 as a year
-			if(film.getYear() != null && film.getYear() <= 2015 && film.getCountryString() != null) {
-				
-				// for each country that is associated with this film
-				for(FilmCountryDB filmCountry : film.getFilmCountryEntities()) {
-					
-					// calculate 
-					if(filmsPerYear.containsKey(filmCountry.getCountryName())) {
-						int[] calculationArray = filmsPerYear.get(filmCountry.getCountryName());
-						
-						calculationArray[filmCountry.getFilm().getYear() - Country.YEAR_OFFSET]++;
-						
-						filmsPerYear.put(filmCountry.getCountryName(), calculationArray);
-					} else {
-						// initialize a new array for all years
-						int[] calculationArray = new int[currentYear - Country.YEAR_OFFSET + 1];
-						
-						calculationArray[filmCountry.getFilm().getYear() - Country.YEAR_OFFSET]++;
-						
-						filmsPerYear.put(filmCountry.getCountryName(), calculationArray);
-					}
-				}
-			}
-		}
-		
-		for(Entry<String, int[]> country : filmsPerYear.entrySet()) {
-			Country c = new Country(country.getKey());
-			c.setNumberOfFilms(country.getValue());
-			countries.add(c);
-		}
-		
-		// fetch the list of filtered country entities from the database
-		/*dbCountries = getCountryEntitiesList(filter);
-
-		// get the current year
 		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
 
-		// convert each CountryDB instance to a Country DataTransferObject
-		for (CountryDB country : dbCountries) {
-			Country c = country.toCountry();
-			
-			// initialize a new array for all years
-			int[] filmsPerYear = new int[currentYear - Country.YEAR_OFFSET + 1];
-
-			// for each film entity, increment it's year in the array
-			for (FilmCountryDB filmCountry : country.getFilmCountryEntities()) {
-				if (filmCountry.getFilm().getYear() != null) {
-					filmsPerYear[filmCountry.getFilm().getYear() - Country.YEAR_OFFSET]++;
-				}
-			}
-
-			// start the number of films array transformation
-			c.setNumberOfFilms(filmsPerYear);
-
-			// add the country to the result list
-			countries.add(c);
-		}*/
-
-		// return the filled list of countries
-		return countries;
-	}
-
-	/**
-	 * Returns a filtered list of country entities
-	 * 
-	 * @author Roland Schläfli
-	 * @pre -
-	 * @post -
-	 * @param FilmFilter
-	 *            filter A filter object
-	 * @return List<CountryDB> dbCountries A filtered list of country entities
-	 */
-	@Transactional
-	public List<CountryDB> getCountryEntitiesList(FilmFilter filter) {
-		List<CountryDB> dbCountries = new ArrayList<CountryDB>();
-		// initialize the selector in the query
-		String selector = "SELECT DISTINCT c FROM CountryDB c";
+		// build the query
+		String selector = "SELECT DISTINCT c.id, c.name, f.year, COUNT(*)";
 
 		// initialize the where string with the basic filters
 		String wheres = "";
 		String whereLength = "";
-		String whereYear = "";
 
 		if (filter.getLengthStart() > 0 || filter.getLengthEnd() < 600) {
-			whereLength = "(f.length BETWEEN :minLength AND :maxLength)";
+			whereLength = "WHERE (f.length BETWEEN :minLength AND :maxLength)";
 			wheres += whereLength;
-		}
-
-		if (filter.getYearStart() > 1890 || filter.getYearEnd() < 2015) {
-			if (wheres.length() > 0) {
-				wheres += " AND ";
-			}
-
-			whereYear = "(f.year BETWEEN :minYear AND :maxYear)";
-			wheres += whereYear;
-		}
-
-		if (wheres.length() > 0) {
-			wheres = "WHERE " + wheres;
 		} else {
 			wheres = "WHERE 1=1";
 		}
 
-		// initialize an empty string for all the joins
-		String joiners = "JOIN FETCH c.filmCountryEntities fc JOIN FETCH fc.primaryKey.film fi";
+		String joiners = "JOIN film_countries fc ON f.id = fc.film_id JOIN countries c ON fc.country_id = c.id";
 
 		// if the name in the filter is set
 		if (filter.getName() != null) {
-			wheres += " AND LOWER(fi.name) LIKE :findName";
+			wheres += " AND LOWER(f.name) LIKE :findName";
 		}
 
 		// if at least one genre is in the list of filter genres
 		if (filter.getGenreIds() != null) {
-			joiners += " JOIN fi.filmGenreEntities fg";
-			wheres += " AND fg.genreId IN :genreIds";
+			joiners += " JOIN film_genres fg ON f.id = fg.film_id";
+			wheres += " AND fg.genre_id IN :genreIds";
 		}
 
 		// if at least one language is in the list of filter languages
 		if (filter.getLanguageIds() != null) {
-			joiners += " JOIN fi.filmLanguageEntities fl";
-			wheres += " AND fl.languageId IN :languageIds";
+			joiners += " JOIN film_languages fl ON f.id = fl.film_id";
+			wheres += " AND fl.language_id IN :languageIds";
 		}
 
-		// build the query string
-		String queryString = selector + " " + joiners + " " + wheres + " ORDER BY c.name";
+		String queryString = selector + " FROM films f " + joiners + " " + wheres
+				+ " GROUP BY c.name, f.year HAVING f.year IS NOT NULL ORDER BY c.name, f.year";
 
-		// select all countries from the database
-		TypedQuery<CountryDB> query = em.get().createQuery(queryString, CountryDB.class);
+		Query query = em.get().createNativeQuery(queryString);
 
-		if (whereLength.length() > 0) {
-			// set the min & max length params
+		if (whereLength.length() > 0) { // set the min & max length params
 			query.setParameter("minLength", filter.getLengthStart());
 			query.setParameter("maxLength", filter.getLengthEnd());
 		}
 
-		if (whereYear.length() > 0) {
-			// set the min & max year params
-			query.setParameter("minYear", filter.getYearStart());
-			query.setParameter("maxYear", filter.getYearEnd());
-		}
-
-		// if the name in the filter is set, set the param
-		if (filter.getName() != null) {
-			// use % to also find incomplete words
+		// if the name in the filter is set, set the param if
+		if (filter.getName() != null) { // use % to also find incomplete words
 			query.setParameter("findName", "%" + filter.getName().toLowerCase() + "%");
 		}
 
-		// if there are genres specified as a list of Integers
+		// if there are genres specified as a list of Integers if
 		if (filter.getGenreIds() != null) {
 			query.setParameter("genreIds", filter.getGenreIds());
 		}
 
-		// if there are languages specified as a list of Integers
+		// if there are languages specified as a list of Integers if
 		if (filter.getLanguageIds() != null) {
 			query.setParameter("languageIds", filter.getLanguageIds());
 		}
 
-		// execute the query
-		dbCountries = query.getResultList();
-		
-		//Query yearCounts = em.get().createNativeQuery("SELECT c.name AS name, f.year AS year, COUNT(*) AS count FROM countries c JOIN film_countries fc ON c.id = fc.country_id JOIN films f ON fc.film_id = f.id GROUP BY c.name, f.year", CountryYearCountDB.class); 
-		//em.get().createQuery("SELECT CountryYearCountDB FROM CountryDB c JOIN c.filmCountryEntities fc JOIN fc.primaryKey.film f GROUP BY c.name, f.year", CountryYearCountDB.class);
-		
-		// return the filtered list of countries
-		return dbCountries;
+		// fetch the result set
+		List<Object[]> yearCounts = query.getResultList();
+
+		// iterate through the entire resulting list
+		int i = 0;
+		while (i < yearCounts.size()) {
+			int[] filmsPerYear = new int[currentYear - Country.YEAR_OFFSET + 1];
+
+			// as long as the next element is the same as the current one, go on
+			int j = 0;
+
+			// always add the first row
+			filmsPerYear[(int) yearCounts.get(i + j)[2] - Country.YEAR_OFFSET] = ((BigInteger) yearCounts.get(i + j)[3])
+					.intValue();
+			j++;
+
+			// as long as the next element's id equals the first one, use the
+			// same array
+			while (i + j < yearCounts.size() && yearCounts.get(i + j - 1)[0] == yearCounts.get(i + j)[0]) {
+				filmsPerYear[(int) yearCounts.get(i + j)[2]
+						- Country.YEAR_OFFSET] = ((BigInteger) yearCounts.get(i + j)[3]).intValue();
+				j++;
+			}
+
+			// hydrate a new country and add it to the result list
+			Country c = new Country((String) yearCounts.get(i)[1]);
+			c.setNumberOfFilms(filmsPerYear);
+			c.setId((int) yearCounts.get(i)[0]);
+			countries.add(c);
+
+			i = i + j + 1;
+		}
+
+		/*
+		 * for(Country c : countries) { System.err.println(c.toString()); }
+		 */
+
+		// return the filled list of countries
+		return countries;
 	}
 
 	/**
@@ -472,22 +388,22 @@ public class FilmListServiceImpl extends RemoteServiceServlet implements FilmLis
 	 */
 	@Override
 	public HashMap<Integer, String> getGenreSelectOption() {
-		if(cachedGenres != null) {
+		if (cachedGenres != null) {
 			return cachedGenres;
 		}
-		
+
 		List<GenreDB> dbGenres = new ArrayList<GenreDB>();
 
 		// select * from the genre table
 		// TODO: query cache .setHint(QueryHints.CACHEABLE, true)
 		dbGenres = em.get().createQuery("FROM GenreDB", GenreDB.class).getResultList();
-		
+
 		HashMap<Integer, String> availableGenres = new HashMap<Integer, String>(dbGenres.size());
 
 		for (GenreDB genre : dbGenres) {
 			availableGenres.put(genre.getId(), genre.getName());
 		}
-		
+
 		cachedGenres = availableGenres;
 
 		return availableGenres;
@@ -506,10 +422,10 @@ public class FilmListServiceImpl extends RemoteServiceServlet implements FilmLis
 	 */
 	@Override
 	public HashMap<Integer, String> getCountrySelectOption() {
-		if(cachedCountries != null) {
+		if (cachedCountries != null) {
 			return cachedCountries;
 		}
-		
+
 		List<CountryDB> dbCountries = new ArrayList<CountryDB>();
 
 		// select * from the country table
@@ -523,7 +439,7 @@ public class FilmListServiceImpl extends RemoteServiceServlet implements FilmLis
 		}
 
 		cachedCountries = availableCountries;
-		
+
 		return availableCountries;
 	}
 
@@ -540,10 +456,10 @@ public class FilmListServiceImpl extends RemoteServiceServlet implements FilmLis
 	 */
 	@Override
 	public HashMap<Integer, String> getLanguageSelectOption() {
-		if(cachedLanguages != null) {
+		if (cachedLanguages != null) {
 			return cachedLanguages;
 		}
-		
+
 		List<LanguageDB> dbLanguages = new ArrayList<LanguageDB>();
 
 		// select * from the language table
@@ -555,7 +471,7 @@ public class FilmListServiceImpl extends RemoteServiceServlet implements FilmLis
 		for (LanguageDB language : dbLanguages) {
 			availableLanguages.put(language.getId(), language.getName());
 		}
-		
+
 		cachedLanguages = availableLanguages;
 
 		return availableLanguages;
